1924e1fb3a106ab753f609b767f0b4511b43120c
Swairik <swairikdey1001@gmail.com>
2021-12-03 15:58:00 +0530

    added Day3

---
+++ Day3/a.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double lld;
+
+#define fo(n)       for(int i = 0; i < (int)n; i++)
+#define rep(i,x,n)  for(auto i = x; i < n; i++)
+#define pf          push_front
+#define pb          push_back
+#define ppf         pop_front
+#define ppb         pop_back
+#define ff          first
+#define ss          second
+#define mem0(a)     memset(a,0,sizeof(a))
+#define all(v)      v.begin(),v.end()
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define ch          " "
+#define nline       "\n"
+#define LIGHTS ios_base::sync_with_stdio(false); 
+#define OUT cin.tie(NULL);
+
+const int INF  =  1e9 + 20;
+const ll INFLL =  2e18 + 20;
+const lld pi   =  3.14159265358979323846;
+
+const int MOD  =  1e9 + 7;
+const int MAX  =  1e5 + 20;
+
+void usaco(string filename) {
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+
+#define int ll
+
+int convert(string s) {
+    int n = (int)s.size();
+    int ans = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        if(s[i] == '1') {
+            ans |= (1 << (n - i - 1));
+        }
+    }
+    return ans;
+}
+
+void solve() {
+    vector<string> s;
+    string temp;
+    while(cin >> temp) {
+        s.push_back(temp);
+    }
+    string gamma = "", epsilon = "";
+    for(int i = 0; i < (int)s[0].size(); i++) {
+        int z = 0, o = 0;
+        for(int j = 0; j < (int)s.size(); j++) {
+            if(s[j][i] == '0')  z++;
+            else    o++;
+        }
+        if(o > z) {
+            gamma += '1';
+            epsilon += '0';
+        }
+        else {
+            gamma += '0';
+            epsilon += '1';
+        }
+    }
+    int ep = convert(epsilon);
+    int ga = convert(gamma);
+    cout << ep * ga << nline;
+}      
+
+signed main() {
+    LIGHTS OUT;
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    // #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);   
+        freopen("out.txt","w",stdout);
+    // #endif
+
+    // usaco("");
+
+    int t = 1;
+    // cin >> t;
+
+    for(int i = 1; i <= t; i++){
+        // cout << "Case #" << i << ": ";
+        // cout << nline;
+        solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+    // double time_taken = chrono::duration_cast<chrono::nanoseconds>(end-start).count();
+    // time_taken *= 1e-9;
+    // cerr << "time taken: " << fixed << time_taken << setprecision(9) << endl;   
+}
